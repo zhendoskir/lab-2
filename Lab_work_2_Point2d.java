fcd21f6e8e74240aafbbcba684dc534aecbdc63f
zhendoskir <83293340+zhendoskir@users.noreply.github.com>
2021-04-27 17:45:35 +0300

    Create Lab_work_2_Point2d.java

---
+++ Lab_work_2_Point2d.java
@@ -0,0 +1,38 @@
+/*** двумерный класс точки. **/
+public class Point2d {
+
+    private double xCoord;
+    private double yCoord;
+
+    public Point2d(double x, double y) {
+        xCoord = x;
+        yCoord = y;
+    }
+
+    public Point2d() {
+        this(0, 0);
+    }
+
+    public double getX() {
+        return xCoord;
+    }
+
+    public double getY() {
+        return yCoord;
+    }
+
+    public void setX(double val) {
+        xCoord = val;
+    }
+
+    public void setY(double val) {
+        yCoord = val;
+    }
+
+    public boolean EqualsPoint(Point2d second){
+        if (this.getX() == second.getX() && this.getY() == second.getY()){
+            return true;
+        }
+        return false;
+    }
+}
